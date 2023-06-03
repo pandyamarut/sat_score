f64d6f16fa5c8d2a25ed6af107c0001c91e017fd
pandyamarut <pandyamarut@gmail.com>
2023-06-03 12:05:18 -0500

    Fixes The getRank funtion
    Signed-off-by: pandyamarut <pandyamarut@gmail.com>

--- endpoints/get_rank.go
+++ endpoints/get_rank.go
@@ -7,11 +7,15 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-// GetRank returns the rank of a student
+// GetRank returns the rank of the entry with the given name
 func GetRank(c *fiber.Ctx, db *gorm.DB) error {
 	name := c.Params("name")
 	var count int
-	db.Model(&types.SATResult{}).Where("name < ?", name).Count(&count)
-	return c.JSON(fiber.Map{"rank": count + 1})
-
+	var satResult types.SATResult
+	//searched for the entry with the name and then found the count of the entries with a higher score
+	db.Model(&types.SATResult{}).Where("name = ?", name).First(&satResult)
+	// counts the number of rows with a higher score relative to the scoe of the name passed.
+	db.Model(&types.SATResult{}).Where("sat_score > ?", satResult.SATScore).Count(&count)
+	rank := count + 1 // adds one to the count to get the rank
+	return c.JSON(fiber.Map{"rank==": rank})
 }
